e577f9f1d18d2f994a8f026bb4facf76682b3091
Fabio Amadio <fabioamadio93@gmail.com>
2026-02-23 09:13:17 +0100

    FIX to_jit_task.sh

--- to_jit_task.sh
+++ to_jit_task.sh
@@ -8,7 +8,7 @@ exptid=${1}
 proj_name="g1_stu_rl_task"
 
 # Run the training script
-python save_jit_stu_rlbc_task.py --robot "g1" \
+python save_jit_stu_rlbc_task.py \
                 --proj_name "${proj_name}" \
                 --exptid "${exptid}" \
                 --checkpoint -1 \
